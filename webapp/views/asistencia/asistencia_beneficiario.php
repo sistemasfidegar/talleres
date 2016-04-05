39a62b9508e37c7f322441658cc342073182f2ad
icony <conyj.montalvan@gmail.com>
2016-04-05 18:04:16 -0500

    se sube el módulo de asistencia

---
+++ webapp/views/asistencia/asistencia_beneficiario.php
@@ -0,0 +1,214 @@
+
+<script type="text/javascript">
+        jQuery(document).ready(function(){
+
+
+    		$("#guardar").click(function () {
+    			if($("#matricula_asignada").val() != ""  ) 
+    	        {
+    				$.blockUI({message: 'Procesando por favor espere...'});
+    	        	jQuery.ajax({
+    		            type: 'post',
+    		            dataType: 'html',
+    		            url: 'asistencia/registroAsistencia',
+    		            data: {matricula: $("#matricula_asignada").val()},
+    		            success: function (data) {
+        		            if(data == 'registrado') {
+        		            	$.unblockUI();
+        		            	$('#myModalRegistrado').modal('show'); //open modal
+        		            	$("#matricula_asignada").val('');
+            		        }
+        		            else  if(data == 'bad'){
+        		            	$.unblockUI();
+        		            	$('#myModalSinRegistro').modal('show'); //open modal
+            		        }
+        		            else{
+        		            	$.unblockUI();
+        		            	$('#myModalRegistro').modal('show'); //open modal
+        		            	$('#mensaje').html('Asistencia '+data+' Completo');
+        		            	$("#matricula_asignada").val('');
+        		            }
+            		            
+    		            }
+    		            
+    		        });
+    	        } /*else if($("#matricula_escuela").val()!= "" ){
+    				$.blockUI({message: 'Procesando por favor espere...'});
+    	        	jQuery.ajax({
+    		            type: 'post',
+    		            dataType: 'html',
+    		            url: 'asistencia/ajax_beneficiario_unam/',
+    		            data: {matricula_escuela: $("#matricula_escuela").val()},
+    		            success: function (data) {
+    		            	 if(data != 'bad') {
+             		            irA('asistencia/nuevo/'+ data);
+         		            } else {
+         		            	$.unblockUI();
+         		            	$('#myModalSinRegistro').modal('show'); //open modal
+             		        }
+    		            }
+    		        });
+    			}*/
+    		});
+        });//ready
+        function irA(uri) {
+            window.location.href =  uri;
+            
+        }	
+</script>
+
+<div class="modal fade" tabindex="-1" role="dialog" id="myModalSinRegistro">
+		<div class="modal-dialog modal-sm">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" align="center">Beneficiario Sin Registro</h4>
+				</div>
+				<div class="modal-body">
+					<form id="attributeForm" role="form">
+						<div class="form-group">
+							Lo sentimos no se encontró tu registro a los talleres Prepa Sí
+							<br/>
+							<br/>1. Verifica  que tu matrícula esté escrita correctamente 
+							<br/><center><img src="resources/img/pink-sad-face.png" alt="codesi" width="50%" class="img-rounded"></center>
+							
+						</div>
+					</form>
+				</div>
+				
+			</div><!-- /.modal-content -->
+		</div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="myModalRegistro">
+		<div class="modal-dialog modal-sm">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" align="center">Bienvenido</h4>
+				</div>
+				<div class="modal-body">
+					<form id="attributeForm" role="form">
+						<div class="form-group">
+							 <label for="mensaje" class="control-label"><div id="mensaje"></div></label><br/><br/>
+							 <br/><center><img src="resources/img/ok.png" alt="codesi" width="50%" class="img-rounded"></center>
+						</div>
+					</form>
+				</div>
+		
+			</div><!-- /.modal-content -->
+		</div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->	
+	<div class="modal fade" tabindex="-1" role="dialog" id="myModalRegistrado">
+		<div class="modal-dialog modal-sm">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" align="center">¡El beneficiario ya se registró!</h4>
+				</div>
+				<div class="modal-body">
+					<form id="attributeForm" role="form">
+						<div class="form-group">
+							 <br/><center><img src="resources/img/pink-happy-face.png" alt="codesi" width="50%" class="img-rounded"></center>
+						</div>
+					</form>
+				</div>
+		<!--  	<div class="modal-footer">
+					<center>
+						Para mayor informaci&oacute;n visita:<br/>
+						<a href="http://www.prepasi.df.gob.mx" target="_blank">www.prepasi.df.gob.mx</a><br/>
+						<a href="https://www.facebook.com/pprepasi" target="_blank">
+							<span class="fa-stack fa-lg">
+                            	<i class="fa fa-circle fa-stack-2x"></i>
+                                <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </a>
+                        <a href="https://www.twitter.com/P_Prepa_Si" target="_blank">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                        </a>
+                        <a href="https://www.instagram.com/actividadesps/" target="_blank">
+                                <span class="fa-stack fa-lg">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-instagram fa-stack-1x fa-inverse"></i>
+                                </span>
+                        </a><br/>
+						Atenci&oacute;n telef&oacute;nica Prepa S&iacute; 1102 1750 (L a V de 9 a 18 hrs)
+					</center>
+				</div>-->
+			</div><!-- /.modal-content -->
+		</div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->	
+<div class="register-container container">
+	<div class="row">                
+		<div class="register">
+			<form role="form" id="buscar_beneficiario" name="buscar_beneficiario" action="" method="post" autocomplete="off">
+				 <div style="text-align:left; padding-left:20px; border-bottom: 2px dotted #bbb; min-height:73px;">
+                 	<a href="http://www.prepasi.df.gob.mx/">	<img  src="resources/formulario/img/logo_gdf_fidegar.png" style="padding-top:10px;" align="top" />&nbsp;</a>
+                 </div>
+                 <?php	if (isset($disponible)){ 
+                 			if($disponible == 1) { ?>
+				<div class="form-goup">
+					<br>
+					<table width="100%" border="0">		                        	
+			        	<tr>
+			        		<td>NO HAY TALLERES POR IMPARTIR EN ESTE MOMENTO</td>		                        		
+			            </tr>
+			            <tr><td>&nbsp;&nbsp;</td></tr>
+			            <tr><td>&nbsp;&nbsp;</td></tr>
+			            <tr>
+				            <td>
+					            <div style="text-align:rigth; padding-left:20px;  min-height:73px;" class="span4">
+		                			<a href="http://www.prepasi.df.gob.mx/" class="btn">Terminar</a>                                         	
+		                		</div>
+		                	</td>
+	                	</tr>
+			        </table>       	
+					</div>	
+					<?php } ?>				
+				<?php } else { ?>
+                 <br>
+                 	<div style="text-align:CENTER !important;"><label class="leyenda" style="color:#E6007E; padding-left:20px;"> ASISTENCIA TALLERES PREPA SÍ </label></div>
+						<table width="620" border="0" align="center" cellpadding="0" cellspacing="0">
+							 <tr>
+							   	<td bgcolor="">
+							    	<table width="95%" border="0" align="center" cellpadding="0" cellspacing="5">
+							    	    <tr>
+							        		<td colspan="2" align="center" class=""></td>
+							          	</tr>      
+							        	 <tr>
+							         		<td colspan="2">&nbsp;</td>
+								        </tr>
+								        <tr>
+								          <td colspan="2"><input type="text" id="matricula_asignada" name="matricula_asignada" value="" placeholder="                 Ingresa tu matricula PS o CURP" style="width:80%; text-transform:uppercase;"/></td>
+							        	</tr>
+								         <tr>
+								          <td colspan="2">&nbsp;</td>
+								        </tr>
+								         <tr>
+									      <td colspan ="2""center">
+								          	  <div class="box-footer" style="text-align: center;" >
+							     				<button style="width:50%;" id="guardar" name="guardar" type="button" class="btn">Consultar</button>
+							     		   	  </div>
+							        	  </td>
+								        </tr>
+								        <tr>
+								          <td>&nbsp;</td>
+							         	  <td>&nbsp;</td>
+							        	</tr>
+							        	<tr>
+							        	<td>&nbsp;</td>
+									  	<td><div style="width:; display:inline-block;" id="letrero"> </div></td>
+									    <td>&nbsp;</td>
+							        	</tr>
+							      	</table>
+							 </tr>
+						</table>
+				<?php } ?>
+						
+				</form>
+			</div>
+		</div>
+	</div>
+		 							
